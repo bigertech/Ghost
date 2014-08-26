ee520c30e3d1e972872265a2546b6dfac02c4a8d
orangehat36 <orangehat.rb@gmail.com>
2014-08-26 17:39:57 +0800

    Commit f2e code

--- content/themes/bigertech/assets/js/index.js
+++ content/themes/bigertech/assets/js/index.js
@@ -1,90 +1,21 @@
-/**
- * Main JS file for Casper behaviours
- */
-
-/*globals jQuery, document */
-(function ($) {
-    "use strict";
-
-    $(document).ready(function(){
-
-        $(".post-content").fitVids();
-
-        function casperFullImg() {
-            $("img").each( function() {
-                var contentWidth = $(".post-content").outerWidth(); // Width of the content
-                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
-
-                if (imageWidth >= contentWidth) {
-                    $(this).addClass('full-img');
-                } else {
-                    $(this).removeClass('full-img');
-                }
-            });
-        };
-
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
-
-        $(".scroll-down").arctic_scroll();
-
-    });
-
-}(jQuery));
-
-(function($,sr){
-
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  var debounce = function (func, threshold, execAsap) {
-      var timeout;
-
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
-
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
-
-          timeout = setTimeout(delayed, threshold || 100);
-      };
+jQuery(function($) {
+  if (navigator.userAgent.indexOf("MSIE") < 0) {
+      //IE8 不支持console
+      console.log("^_^ 想加入笔戈科技开发团队？请发送邮件到 liuxing@meizu.com");
   }
-  // smartresize 
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
-})(jQuery,'smartresize');
+  if($("img.lazy") && $("img.lazy").length > 0)
+    $("img.lazy").lazyload({effect: "fadeIn"});
 
-// Arctic Scroll by Paul Adam Davis
-// https://github.com/PaulAdamDavis/Arctic-Scroll
-(function ($) {
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        };
-        var options = $.extend(defaults, options);
-
-        options.elem.click(function(event){     
-            event.preventDefault();
-            var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
-                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;         
-            if (offset) {
-                var toMove = parseInt(offset);
-                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
-            } else if (position) {
-                var toMove = parseInt(position);
-                $('html,body').stop(true, false).animate({scrollTop: toMove }, options.speed);
-            } else {
-                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, options.speed);
-            }
-        });
-
-    };
-})(jQuery);
+  $(document).ready( function() {
+      var qrcode = $(".qrcode");
+      $(".icon-wechat").hover(function(){
+          var pos = $(this).position();
+          qrcode.css("left", ($(window).width()-qrcode.width())/2);
+          qrcode.css("top", pos.top-270);
+          qrcode.fadeIn().show();
+      },function(){
+          qrcode.hide();
+      });
+  });
+});
