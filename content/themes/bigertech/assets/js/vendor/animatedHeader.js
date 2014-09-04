9ed7e7d608efe902a4f9beed9afa5ee009024f7f
orangehat36 <orangehat.rb@gmail.com>
2014-09-04 16:32:07 +0800

    add animationHeader.js

---
+++ content/themes/bigertech/assets/js/vendor/animatedHeader.js
@@ -0,0 +1,49 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var animatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		navbar = document.querySelector( '.navbar' ),
+		didScroll = false,
+		changeHeaderOn = 312;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( navbar, 'navbar-pinned' );
+		}
+		else {
+			classie.remove( navbar, 'navbar-pinned' );
+		}
+
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	if (document.body.clientWidth > 768 ) {
+		if ( document.querySelector( '.home-template' ) ) {
+			init();
+		}
+	}
+
+})();
