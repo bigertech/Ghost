2f05f606502f00c62aaf3070e1572aa1d76ed1a2
liu.xing <kissliux@qq.com>
2014-09-15 19:05:52 +0800

    位置设置权限管理，邀请邮件修改

--- core/test/bigertech/image_sm.js
+++ core/test/bigertech/image_sm.js
@@ -4,5 +4,5 @@
 var image_url = '/content/images/2014/Jul/1-1.jpg';
 var last_slug = image_url.lastIndexOf('\/');
 
-var result = image_url.substr(0,last_slug)+'/image_sm/'+image_url.substr(last_slug,image_url.length);
+var result = image_url.substr(0,last_slug)+'/image_sm'+image_url.substr(last_slug,image_url.length);
 console.log(result);
