d86e44596c7da20f8952e113225fc7daaf823b14
Aaaaaashu <orangehat.rb@gmail.com>
2014-10-23 11:03:43 +0800

    add index.js and classie.js

--- content/themes/bigertech/assets/js/index.js
+++ content/themes/bigertech/assets/js/index.js
@@ -1,7 +1,11 @@
+/**
+ * Created by Ashu on 23/10/14.
+ */
+
 jQuery(function($) {
   if (navigator.userAgent.indexOf("MSIE") < 0) {
-      //IE8 不支持console
-      console.log("^_^ 想加入笔戈科技开发团队？请发送邮件到 liuxing@meizu.com");
+    //IE8 不支持console
+    console.log("^_^ 想加入笔戈科技开发团队？请发送邮件到 liuxing@meizu.com");
   }
 
   if($("img.lazy") && $("img.lazy").length > 0)
@@ -11,14 +15,14 @@ jQuery(function($) {
     });
 
   $(document).ready( function() {
-      var qrcode = $(".qrcode");
-      $(".icon-wechat").hover(function(){
-          var pos = $(this).position();
-          qrcode.css("left", ($(window).width()-qrcode.width())/2);
-          qrcode.css("top", pos.top-270);
-          qrcode.fadeIn().show();
-      },function(){
-          qrcode.hide();
-      });
+    var qrcode = $(".qrcode");
+    $(".icon-wechat").hover(function(){
+      var pos = $(this).position();
+      qrcode.css("left", ($(window).width()-qrcode.width())/2);
+      qrcode.css("top", pos.top-270);
+      qrcode.fadeIn().show();
+    },function(){
+      qrcode.hide();
+    });
   });
 });
