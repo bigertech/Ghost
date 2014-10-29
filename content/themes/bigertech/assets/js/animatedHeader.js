ec9844773c1944ed8d565a9c5fb19f98ef17187c
Aaaaaashu <orangehat.rb@gmail.com>
2014-10-29 15:34:24 +0800

    PM need and repair IE 7 alert

--- content/themes/bigertech/assets/js/animatedHeader.js
+++ content/themes/bigertech/assets/js/animatedHeader.js
@@ -9,82 +9,83 @@
  * http://www.codrops.com
  */
 
-var animatedHeader = (function() {
+var isIE = function(ver){
+  var b = document.createElement('b')
+  b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->'
+  return b.getElementsByTagName('i').length === 1
+}
 
-	var docElem = document.documentElement,
-		  navbar = document.querySelector( '.navbar' ),
-		  didScroll = false,
-		  changeHeaderOn = 312;
+if(isIE(7)){
+  alert("你的浏览器版本太低，请尽快升级,以便获得更好的浏览体验");
+} else {
+  var animatedHeader = (function() {
 
-	function init() {
-    if (window.addEventListener) {
-      window.addEventListener( 'scroll', function( event ) {
-        if( !didScroll ) {
-          didScroll = true;
-          setTimeout( scrollPage, 250 );
-        }
-      }, false );
-    } else {
-      window.attachEvent( 'onscroll', function( event ) {
-        if ( !didScroll ) {
-          didScroll = true;
-          setTimeout( scrollPage, 250 );
-        }
-      });
+    var docElem = document.documentElement,
+      navbar = document.querySelector( '.navbar' ),
+      didScroll = false,
+      changeHeaderOn = 312;
+
+    function init() {
+      if (window.addEventListener) {
+        window.addEventListener( 'scroll', function( event ) {
+          if( !didScroll ) {
+            didScroll = true;
+            setTimeout( scrollPage, 250 );
+          }
+        }, false );
+      } else {
+        window.attachEvent( 'onscroll', function( event ) {
+          if ( !didScroll ) {
+            didScroll = true;
+            setTimeout( scrollPage, 250 );
+          }
+        });
+      }
     }
-	}
 
-//  function init() {
-//    if (window.addEventListener) {
-//      window.addEventListener( 'scroll', function( event ) {
-//        if( !didScroll ) {
-//          didScroll = true;
-//          setTimeout( scrollPage, 250 );
-//        }
-//      }, false );
-//    }
-//  }
+    function scrollPage() {
+      var sy = scrollY();
+      if ( sy >= changeHeaderOn ) {
+        classie.add( navbar, 'navbar-pinned' );
+      }
+      else {
+        classie.remove( navbar, 'navbar-pinned' );
+      }
 
-	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( navbar, 'navbar-pinned' );
-		}
-		else {
-			classie.remove( navbar, 'navbar-pinned' );
-		}
+      didScroll = false;
+    }
 
-		didScroll = false;
-	}
+    function scrollY() {
+      return window.pageYOffset || docElem.scrollTop;
+    }
 
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
+    if ( document.querySelector( '.home-template' ) ) {
+      if (document.body.clientWidth > 740 ) {
+        init();
+      }
+    }
 
-	if ( document.querySelector( '.home-template' ) ) {
-		if (document.body.clientWidth > 740 ) {
-			init();
-		}
-	}
+    if ( document.querySelector( '.category-template' ) ) {
+      if ( document.querySelector( '.archive-template' ) ) {
+        return false;
+      } else {
+        if (document.body.clientWidth > 740 ) {
+          init();
+        }
+      }
+    }
 
-	if ( document.querySelector( '.category-template' ) ) {
-		if ( document.querySelector( '.archive-template' ) ) {
-			return false;
-		} else {
-			if (document.body.clientWidth > 740 ) {
-				init();
-			}
-		}
-	}
+    if ( document.querySelector( '.author-template' ) ) {
+      if ( document.querySelector( '.archive-template' ) ) {
+        return false;
+      } else {
+        if (document.body.clientWidth > 740 ) {
+          init();
+        }
+      }
+    }
 
-	if ( document.querySelector( '.author-template' ) ) {
-		if ( document.querySelector( '.archive-template' ) ) {
-			return false;
-		} else {
-			if (document.body.clientWidth > 740 ) {
-				init();
-			}
-		}
-	}
+  })();
+}
 
-})();
+
